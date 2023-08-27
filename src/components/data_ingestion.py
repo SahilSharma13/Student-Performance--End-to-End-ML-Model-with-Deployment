d396407303e5b7dc3fc5cd51ad921c5534095f57
SahilSharma13 <sharmaz.sahil13@gmail.com>
2023-08-28 01:56:24 +0530

    Prediction Pipe

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -57,6 +57,6 @@ if __name__=="__main__":
     data_transformation=DataTransformation()
     train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
 
-    ModelTrainer=ModelTrainer()
-    print(ModelTrainer.initiate_model_trainer(train_arr,test_arr))
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
     
